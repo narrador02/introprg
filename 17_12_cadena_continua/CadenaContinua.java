34887925457f6d46ef00aad52b5294d47c53ad10
Jairo <2023_jairo.vigil@iticbcn.cat>
2025-01-13 15:03:46 +0100

    Ejercicio completado

---
+++ 17_12_cadena_continua/CadenaContinua.java
@@ -0,0 +1,18 @@
+/* Nueva versión del programa 31_75 con estructura modular. Hace uso del fichero UtilString.java en dos ocasiones */
+public class CadenaContinua {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        if (text.isEmpty()) {
+            System.out.println("error");
+        } else {
+            System.out.println("Nombre?");
+            String nombre = Entrada.readLine();
+            if (UtilString.esEnter(nombre)) {
+                System.out.println(UtilString.cadenaContinua(text, Integer.parseInt(nombre)));
+            } else {
+                System.out.println("error");
+            }
+        }
+    }
+}
