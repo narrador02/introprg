caab3ad4657f9df9c99e45d5c68c5f2b5dc9ab4e
Jairo <2023_jairo.vigil@iticbcn.cat>
2025-01-24 12:42:07 +0100

    Capcalera actualizada

--- 17_13_substring/Main.java
+++ 17_13_substring/Main.java
@@ -1,4 +1,6 @@
-/* Aquest programa comprova el funcionament de la funció UtilSTring.esSubstring() */
+/*
+ * Aquest programa comprova el funcionament de la funció UtilSTring.esSubstring()
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Text principal?");
