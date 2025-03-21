ab3200b9ae1942560e441e12fa1328fa4ed02b44
Jairo <2023_jairo.vigil@iticbcn.cat>
2025-03-21 12:43:51 +0100

    Ejercicio completado

---
+++ 30_10_posicio/GatRenat.java
@@ -0,0 +1,7 @@
+/*
+ * Programa el cual contiene valores de las vidas y la posición del gato que se hará servir en un UsaGatRenat.java
+ */
+ public class GatRenat {
+    int vides = 7;
+    String posicio = "estirat";
+}
---
+++ 30_10_posicio/UsaGatRenat.java
@@ -0,0 +1,12 @@
+/*
+ * Programa que hace uso de la clase GatRenat para obtener los valores que necesita
+ */
+ public class UsaGatRenat {
+    public static void main(String[] args) {
+        GatRenat renat = new GatRenat();
+        System.out.println("Vides inicials: " + renat.vides);
+        System.out.println("Posició inicial: " + renat.posicio);
+        renat.posicio = "assegut";
+        System.out.println("Posició final: " + renat.posicio);
+    }
+}
