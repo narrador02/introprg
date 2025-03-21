82b5ea930cf953565d0d321750b17057dcae554f
Jairo <2023_jairo.vigil@iticbcn.cat>
2025-03-21 12:40:25 +0100

    Ejercicio completado

---
+++ 30_09_extern/Ascensor.java
@@ -0,0 +1,6 @@
+/*
+ * Programa que contiene el valor del piso que se hará servir en un UsaAscensor.java
+ */
+public class Ascensor {
+    int pis = -1;
+}
---
+++ 30_09_extern/UsaAscensor.java
@@ -0,0 +1,9 @@
+/*
+ * Programa que hace uso de la clase Ascensor para mostrar el valor de piso 
+ */
+public class UsaAscensor {
+    public static void main(String[] args) {
+        Ascensor ascensor = new Ascensor();
+        System.out.println("L'ascensor creat des de fora està al pis " + ascensor.pis);
+    }
+}
