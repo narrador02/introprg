268255f00f378a8e02a54c55edfc7e5042c42983
Jairo <2023_jairo.vigil@iticbcn.cat>
2025-04-10 00:14:29 +0200

    Ejercicio completado

--- 30_13_posicio_correcta/UsaGatRenat.java
+++ 30_13_posicio_correcta/UsaGatRenat.java
@@ -5,7 +5,7 @@
 public class UsaGatRenat {
     public static void main(String[] args) {
         GatRenat renat = new GatRenat();
-        System.out.println("Vides inicials: " + renat.getVides());
+        System.out.println("Les vides inicials són: " + renat.getVides());
         System.out.println("Posició inicial: " + renat.getPosicio());
         System.out.println("Introdueix nova posició:");
         renat.setPosicio(Entrada.readLine());
