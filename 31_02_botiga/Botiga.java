b0429d07e765d3fc56a4ae07ca8ded3b49b4f1ce
Jairo <2023_jairo.vigil@iticbcn.cat>
2025-04-26 19:05:38 +0200

    Ejercicio completado

---
+++ 31_02_botiga/Botiga.java
@@ -0,0 +1,67 @@
+/* 
+ * Programa que permite gestionar la colección de vinos
+ * que hay en la botiga, verificando su stock y disponibilitad
+ */
+public class Botiga {
+    private static int DEFAULT_MAX_VINS = 10;
+    private Vi[] vins;
+
+    public Botiga() {
+        vins = new Vi[DEFAULT_MAX_VINS];
+    }
+
+    public Botiga(int maxVins) {
+        if (maxVins < 1) {
+            vins = new Vi[DEFAULT_MAX_VINS];
+        } else {
+            vins = new Vi[maxVins];
+        }
+    }
+
+    public Vi afegeix(Vi vi) {
+        if (vi.esValid()) {
+            for (int i = 0; i < vins.length; i++) {
+                if (vins[i] != null && vins[i].getNom().equals(vi.getNom())) {
+                    return null;
+                }
+            }
+            for (int i = 0; i < vins.length; i++) {
+                if (vins[i] == null) {
+                    vins[i] = vi;
+                    return vi;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Vi elimina(String nom) {
+        String nomVi = Vi.normalitzaNom(nom);
+        for (int i = 0; i < vins.length; i++) {
+            if (vins[i] != null && vins[i].getNom().equals(nomVi)) {
+                if (vins[i].getEstoc() > 0) {
+                    return null;
+                } else {
+                    Vi vi = vins[i];
+                    vins[i] = null;
+                    return vi;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Vi cerca(String nom) {
+        String nomVi = Vi.normalitzaNom(nom);
+        for (int i = 0; i < vins.length; i++) {
+            if (vins[i] != null) {
+                String vi = vins[i].getNom().toLowerCase();
+                nomVi = nomVi.toLowerCase();
+                if (vi.equals(nomVi)) {
+                    return vins[i];
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ 31_02_botiga/Vi.java
@@ -0,0 +1,87 @@
+/*
+ * Clase Vi que contiene todas las propiedades
+ * necesarias, sus constructores y métodos
+ */
+public class Vi {
+    private String nom;
+    private int preu;
+    private int estoc = 0;
+    
+    public Vi(String nom, int preu) {
+        this.nom = normalitzaNom(nom);
+        if (preu < 0) {
+            this.preu = -1;
+        } else {
+            setPreu(preu);
+        }
+    }
+    
+    public Vi(String nom, int preu, int estoc) {
+        this.nom = normalitzaNom(nom);
+        if (preu < 0) {
+            this.preu = -1;
+        } else {
+            setPreu(preu);
+        }
+        if (estoc < 0) {
+            this.estoc = -1;
+        } else {
+            setEstoc(estoc);
+        }
+    }
+    
+    public String getNom() { return nom; }
+    
+    public int getPreu() { return preu; }
+    
+    public int getEstoc() { return estoc; }
+    
+    public void setPreu(int preu) {
+        if (preu > 0) {
+            this.preu = preu;
+        }
+    }
+    
+    public void setEstoc(int estoc) {
+        if (estoc > 0) {
+            this.estoc = estoc;
+        }
+    }
+    
+    public boolean esValid() {
+        if (getNom().equals("NOM NO VÀLID!") || preu < 0 || estoc < 0) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static String normalitzaNom(String nom) {
+        nom = nom.trim();
+        String resultat = "";
+        int cont = 0;
+        for (int i = 0; i < nom.length(); i++) {
+            char caracter = nom.charAt(i);
+            if (cont == 0 && Character.isWhitespace(caracter)) {
+                cont++;
+                resultat = resultat + nom.charAt(i);
+                continue;
+            } else if (Character.isWhitespace(caracter)) {
+                continue;
+            }
+            cont = 0;
+            resultat = resultat + nom.charAt(i);
+        }
+        if (resultat.isBlank()) {
+            resultat = "NOM NO VÀLID!";
+        }
+        return resultat;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" +
+               "    Vi: " + nom + "\n" +
+               "    Preu: " + preu + "\n" +
+               "    Estoc: " + estoc + "\n";
+    }
+}
